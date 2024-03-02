6822a36274e54667761b8d362c66cc59990e0ca7
NguyenChanhDat <154238663+NguyenChanhDat@users.noreply.github.com>
2024-03-02 23:33:05 +0700

    Haven't finished solving this problem yet :(

---
+++ Pascal Triangle/myTry.js
@@ -0,0 +1,13 @@
+let nums = 5;
+function pascalTriangle(nums) {
+  let twoDArray;
+  for (let i = 0; i <= nums; i++) {
+    if ((i = 0)) {
+      twoDArray.push([1]);
+    } else if ((i = 1)) {
+      twoDArray.push([1, 1]);
+    } else {
+    }
+  }
+}
+//i haven't finished yet :((
