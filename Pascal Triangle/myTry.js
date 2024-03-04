6b3d9928ab0152296aacd9596e1ce64a9255cbd8
NguyenChanhDat <154238663+NguyenChanhDat@users.noreply.github.com>
2024-03-04 09:50:10 +0700

    Solved, Recursion Method

--- Pascal Triangle/myTry.js
+++ Pascal Triangle/myTry.js
@@ -1,13 +1,18 @@
 let nums = 5;
-function pascalTriangle(nums) {
-  let twoDArray;
-  for (let i = 0; i <= nums; i++) {
-    if ((i = 0)) {
-      twoDArray.push([1]);
-    } else if ((i = 1)) {
-      twoDArray.push([1, 1]);
-    } else {
-    }
+function pascalTriangle(numRows) {
+  if (numRows == 0) {
+    return [];
   }
+  if (numRows == 1) {
+    return [[1]];
+  }
+  prevArray = pascalTriangle(numRows - 1);
+  newArray = new Array(numRows).fill(1);
+  for (let i = 1; i < numRows - 1; i++) {
+    newArray[i] = prevArray[numRows - 2][i - 1] + prevArray[numRows - 2][i];
+  }
+  prevArray.push(newArray);
+  return prevArray;
 }
-//i haven't finished yet :((
+
+console.log(pascalTriangle(nums));
