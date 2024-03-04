e4e12e85dbfe2d2a64ad732a41d3a6c4d7c76e38
NguyenChanhDat <154238663+NguyenChanhDat@users.noreply.github.com>
2024-03-04 16:21:39 +0700

    Eliminate Recursion
    Im so smart :)))

--- Pascal Triangle/myTry.js
+++ Pascal Triangle/myTry.js
@@ -1,18 +1,19 @@
-let nums = 5;
-function pascalTriangle(numRows) {
-  if (numRows == 0) {
-    return [];
+let rowIndex = 3;
+function pascalTriangleII(rowIndex) {
+  let twoDArray = [[]];
+  for (let i = 0; i < rowIndex + 1; i++) {
+    if (i == 0) {
+      twoDArray[i] = [1];
+    }
+    if (i == 1) {
+      twoDArray[i] = [1, 1];
+    }
+    twoDArray[i] = new Array(i + 1).fill(1);
+    for (let j = 1; j < i; j++) {
+      twoDArray[i][j] = twoDArray[i - 1][j - 1] + twoDArray[i - 1][j];
+    }
   }
-  if (numRows == 1) {
-    return [[1]];
-  }
-  prevArray = pascalTriangle(numRows - 1);
-  newArray = new Array(numRows).fill(1);
-  for (let i = 1; i < numRows - 1; i++) {
-    newArray[i] = prevArray[numRows - 2][i - 1] + prevArray[numRows - 2][i];
-  }
-  prevArray.push(newArray);
-  return prevArray;
+  return twoDArray;
 }
-
-console.log(pascalTriangle(nums));
+let resultArray = pascalTriangleII(rowIndex);
+console.log(resultArray);
