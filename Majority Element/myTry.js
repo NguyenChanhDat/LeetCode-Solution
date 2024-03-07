91eca1e25474311ebcd212ff4948eb7db7de2b82
Nguyễn Chánh Đạt <154238663+NguyenChanhDat@users.noreply.github.com>
2024-03-07 10:23:19 +0700

    Much shorter version
    time complexity: O(nlogn)

--- Majority Element/myTry.js
+++ Majority Element/myTry.js
@@ -4,27 +4,8 @@
 // Output: 2
 let nums = [2, 2, 1, 1, 1, 2, 2];
 function majorityElement(nums) {
-  if (nums.length == 1) {
-    return nums[0];
-  }
   nums.sort((a, b) => a - b);
-  console.log(nums);
   let majorityNumber = Math.floor(nums.length / 2);
-  let currentTemp = 1,
-    finalTemp = 0;
-  let i = 0;
-  for (i = 1; i < nums.length; i++) {
-    if (nums[i] == nums[i - 1]) {
-      currentTemp++;
-      if (currentTemp > finalTemp) {
-        finalTemp = currentTemp;
-      }
-    } else {
-      currentTemp = 1;
-    }
-    if (finalTemp > majorityNumber) {
-      return nums[i - 1];
-    }
-  }
+  return nums[majorityNumber];    
 }
 console.log(majorityElement(nums));
